6c48e948f4aa8cf9e5a347b776a12bd7f7c22204
gagan2017-soc <65751237+gagan2017-soc@users.noreply.github.com>
2020-07-12 21:47:01 +0530

    Update page-faq.component.ts
    Faq Page changes

--- src/app/page-faq/page-faq.component.ts
+++ src/app/page-faq/page-faq.component.ts
@@ -1,17 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import {MessageService} from 'primeng/api';
 
 @Component({
   selector: 'app-page-faq',
   templateUrl: './page-faq.component.html',
-  styleUrls: ['./page-faq.component.css']
+  styleUrls: ['./page-faq.component.css'],
+  providers: [MessageService]
 })
 export class PageFaqComponent implements OnInit {
-
-  constructor() {
+  index: number = -1;
+  constructor(private messageService: MessageService) {
     
    }
 
   ngOnInit(): void {
   }
+  onTabClose(event) {
+    this.messageService.add({severity:'info', summary:'Tab Closed', detail: 'Index: ' + event.index})
+}
+
+onTabOpen(event) {
+    this.messageService.add({severity:'info', summary:'Tab Expanded', detail: 'Index: ' + event.index});
+}
+
+openNext() {
+    this.index = (this.index === 3) ? 0 : this.index + 1;
+}
+
+openPrev() {
+    this.index = (this.index <= 0) ? 3 : this.index - 1;
+}
 
 }
