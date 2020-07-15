b8e6878e7b36e9b36651f0c844e884f152234e12
gagan2017-soc <65751237+gagan2017-soc@users.noreply.github.com>
2020-07-15 23:11:38 +0530

    Update auth.js
    Added support for festival

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,6 +3,7 @@ const express = require("express");
 const router = express.Router({ mergeParams: true });
 const authService = require("../Services/auth");
 const transcationService = require("../Services/transcation");
+const festivalcalservice = require("../Services/festivalcal")
 /* User Registration. *///
 router.post("/register", authService.register);
 
@@ -12,10 +13,11 @@ router.post("/login", authService.login);
 router.get("/member", authService.member);
 router.post("/createtrans" ,transcationService.createTranscation);
 router.post("/updatetrans" ,transcationService.updateTranscation);
+router.post("/createFestival" ,festivalcalservice.createfestivalcal);
 
 router.get("/getAllTrans" ,transcationService.getAllTranscation);
 router.get("/findmember/" ,authService.getCurrentUser);
-
+router.get("/getAllFestival",festivalcalservice.getAllfestivalcal );
 
 
 module.exports = router;
