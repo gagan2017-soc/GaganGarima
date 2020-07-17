0aa92f515f3bd7d198878152d7d9d363db54a742
gagan2017-soc <65751237+gagan2017-soc@users.noreply.github.com>
2020-07-18 00:18:38 +0530

    Update festivalcal.js
    updated fies

--- backend/Services/festivalcal.js
+++ backend/Services/festivalcal.js
@@ -1,64 +1,81 @@
-"use strict";
+// const async = require("async");
+const config = require("../Utilities/config").config;
+const festivalcalDAO = require('../DAO/festivalcalDAO');
+const MD5 = require('md5');
+let festivalcal = require('../Models/festivalcal');
 
-var Models = require("../Models/festivalcal");
-
-const getfestivalcal = criteria =>
-  new Promise((resolve, reject) => {
-    Models.find(criteria)
-      .then(client => resolve(client))
-      .catch(err => reject(err));
-  });
-
-const createfestivalcal = objToSave =>
-  new Promise((resolve, reject) => {
-    new Models(objToSave)
-      .save()
-      .then(client => resolve(client))
-      .catch(err => {reject(err);
-         console.log(err);
-      });
-  });
-
-const updatefestivalcal = (criteria, dataToSet, options) =>
-  new Promise((resolve, reject) => {
-    options.lean = true;
-    options.new = true;
-    Models.findOneAndUpdate(criteria, dataToSet, options)
-      .then(client => resolve(client))
-      .catch(err => reject(err));
-  });
-
-const deletefestivalcal = criteria =>
-  new Promise((resolve, reject) => {
-    Models.findOneAndRemove(criteria)
-      .exec()
-      .then(client => resolve(client))
-      .catch(err => reject(err));
-  });
-
-/*   const getUser=
-  new Promise((resolve, reject) => {
-    Models.find((error,data))
-      .exec()
-      .then(client => resolve(client))
-      .catch(err => reject(err));
-  }); */
-
- /* const getUser=
-  new promise((req, res) => {
-    Models.find((error, data) => {
-      if (error) {
-        return next(error)
+/* API to register new user */
+let createfestivalcal = async (req, res) => {
+  if (!req.body.festdt || !req.body.holidayname) {
+    res.status(401).json({message:'Parameters are missing'})
+  } else {
+    try {
+      let criteria = {
+        festdt: req.body.festdt      } 
+      const checktran = await festivalcalDAO.getfestivalcal(criteria);
+      if (checktran && checktran.length==1) {
+        res.status(401).json({message:'Transaction already Exists'})
       } else {
-        res.json(data)
+        let transdate = {
+            festdt: req.body.festdt ? req.body.festdt : "",
+            holidayname: req.body.holidayname ? req.body.holidayname : "",
+        };
+        const addTran = await festivalcalDAO.createfestivalcal(transdate);
+        // console
+        if (addTran) {
+          res.status(200).json({message:'Transcation registered successfully!'})
+        } else {
+          res.status(403).json({message:"Something went wrong"});
+        }
       }
-    })
-  }); */
+    } catch (error) {
+      res.status(404).json({message:"Something went wrong",error:error});
+    }
+  }
+};
 
-module.exports = {
-  updatefestivalcal: updatefestivalcal,
-  createfestivalcal: createfestivalcal,
-  deletefestivalcal: deletefestivalcal,
-  getfestivalcal: getfestivalcal
+let updatefestivalcal = async (req, res) => {
+  if (!req.body.festdt || !req.body.holidayname) {
+    res.status(401).json({message:'Parameters are missing'})
+  } else {
+    try {
+      let criteria = {
+        festdt: req.body.festdt      } 
+      const checktran = await festivalcalDAO.getfestivalcal(criteria);
+      /* if (checktran && checktran.length==1) {
+        res.status(401).json({message:'Transaction already Exists'})
+      } else { */
+        let transdate = {
+            festdt: req.body.festdt ? req.body.festdt : "",
+            holidayname: req.body.holidayname ? req.body.holidayname : "",
+        };
+        if (checktran && checktran.length==1) {
+        const addTran = await festivalcalDAO.updatefestivalcal(criteria,transdate, {upsert: true} );
+        // console
+        if (addTran) {
+          res.status(200).json({message:'Transcation registered successfully!'})
+        } else {
+          res.status(403).json({message:"Something went wrong"});
+        }
+      }
+    } catch (error) {
+      res.status(404).json({message:"Something went wrong",error:error});
+    }
+  }
+};
+let getAllfestivalcal = async (req, res) => {
+    festivalcal.find((error, data) => {
+    if (error) {
+      return next(error)
+    } else {
+      res.json(data)
+      
+    }
+  })
+} ;
+  module.exports = {
+    createfestivalcal: createfestivalcal,
+    getAllfestivalcal: getAllfestivalcal,
+    updatefestivalcal :updatefestivalcal
 };
 
