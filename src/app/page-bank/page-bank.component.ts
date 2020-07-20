d266c7cb6e681360a9c1c610cddd7de523a1987c
gagan2017-soc <65751237+gagan2017-soc@users.noreply.github.com>
2020-07-20 22:32:59 +0530

    Update page-bank.component.ts
    Added Bank age

--- src/app/page-bank/page-bank.component.ts
+++ src/app/page-bank/page-bank.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface PeriodicElement {
+  srno: Number;
+   name: string;
+   contactno: string;
+   address: string;
+   website: string;
+  
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  { srno: 1, name: 'HDFC Bank Ltd',contactno:'020-2755223',address:'Aundh Road',website:'hdfcbank.com'},
+  { srno: 2, name: 'ICICI Bank ',contactno:'020-2755223',address:'Aundh Road',website:'icicibank.com'},
+
+  
+];
 @Component({
   selector: 'app-page-bank',
   templateUrl: './page-bank.component.html',
   styleUrls: ['./page-bank.component.css']
 })
 export class PageBankComponent implements OnInit {
-
+  //displayedColumns: string[] = ['#', 'Name',Contact Number];
+  dataSource = ELEMENT_DATA;
   constructor() { }
 
   ngOnInit(): void {
   }
 
 }
+
