250451ee46644aed6035639437ab236bf84125e3
gagan2017-soc <65751237+gagan2017-soc@users.noreply.github.com>
2020-07-21 19:28:12 +0530

    Update page-doctor.component.ts
    Added Doctor page

--- src/app/page-doctor/page-doctor.component.ts
+++ src/app/page-doctor/page-doctor.component.ts
@@ -1,12 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+export interface PeriodicElement {
+  srno: Number;
+   name: string;
+   contactno: string;
+   address: string;
+   specality: string;
+  
+}
 
+const ELEMENT_DATA: PeriodicElement[] = [
+  { srno: 1, name: 'Dr. Granthi',contactno:'020 - 25890380',address:'Aundh',specality:'Skin Specialist'},
+  { srno: 2, name: 'Dr Sagar Tuse ',contactno:'9421581728',address:'Ojas Ayurved Chikitsalaya, B Wing Sunshine Greens 2, Khadki Railway Station Road, Khadki, ',specality:'Ayurvedic Doctors'},
+
+  
+];
 @Component({
   selector: 'app-page-doctor',
   templateUrl: './page-doctor.component.html',
   styleUrls: ['./page-doctor.component.css']
 })
 export class PageDoctorComponent implements OnInit {
-
+  dataSource = ELEMENT_DATA;
   constructor() { }
 
   ngOnInit(): void {
