e6e9b8fbdc6ed6c8c6fc4baa8855f1eabffb1a23
gagan2017-soc <65751237+gagan2017-soc@users.noreply.github.com>
2020-07-21 19:41:10 +0530

    Update page-school.component.ts
    Added School page

--- src/app/page-school/page-school.component.ts
+++ src/app/page-school/page-school.component.ts
@@ -1,12 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface PeriodicElement {
+   name: string;
+   contactno: string;
+   address: string;
+   category: string;
+  
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  { name: 'DAV Public School',contactno:'020 - 25890380',address:'Aundh',category:'CBSE nur-12'},
+  { name: 'City International ',contactno:'020-222222',address:'Aundh, ',category:'CBSE nur-12'},
+
+  
+];
 @Component({
   selector: 'app-page-school',
   templateUrl: './page-school.component.html',
   styleUrls: ['./page-school.component.css']
 })
 export class PageSchoolComponent implements OnInit {
-
+  dataSource = ELEMENT_DATA;
   constructor() { }
 
   ngOnInit(): void {
