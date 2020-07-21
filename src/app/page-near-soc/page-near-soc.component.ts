45d5d1417ddf168c62b17a6aa5e88e091a043ac9
gagan2017-soc <65751237+gagan2017-soc@users.noreply.github.com>
2020-07-21 20:03:58 +0530

    Update page-near-soc.component.ts
    Added Near by soc

--- src/app/page-near-soc/page-near-soc.component.ts
+++ src/app/page-near-soc/page-near-soc.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface PeriodicElement {
+  name: string;
+  contactno: string;
+  address: string;
+  comment: string;
+ 
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+ { name: 'Montvert prestine',contactno:'020 - 222225890380',address:'Aundh Road',comment:''},
+ { name: 'Laboni Tower ',contactno:'020-222222',address:'Aundh Road, ',comment:''},
+
+ 
+];
 @Component({
   selector: 'app-page-near-soc',
   templateUrl: './page-near-soc.component.html',
   styleUrls: ['./page-near-soc.component.css']
 })
 export class PageNearSocComponent implements OnInit {
-
+  dataSource = ELEMENT_DATA;
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
 }
